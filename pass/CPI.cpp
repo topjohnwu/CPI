5d9c1fa159000f571ac75364cb07862b9e7374d9
topjohnwu <topjohnwu@gmail.com>
2018-12-06 07:10:38 -0500

    Simple CPS + Memory Separation runtime library done

--- pass/CPI.cpp
+++ pass/CPI.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
@@ -5,6 +7,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 #define DEBUG_TYPE "cpi"
 
@@ -17,8 +20,17 @@ struct CPI : public ModulePass {
     bool runOnModule(Module &M) override {
         LLVMContext &ctx = M.getContext();
 
+        // Commonly used types
+        Type *intT = Type::getInt32Ty(ctx);
+        voidT = Type::getVoidTy(ctx);
+        voidPT = Type::getInt8PtrTy(ctx);
+        voidPPT = PointerType::get(voidPT, 0);
+
         // Add function references in libsafe_rt
-        test = cast<Function>(M.getOrInsertFunction("test", FunctionType::getVoidTy(ctx)));
+        smAlloca = cast<Function>(M.getOrInsertFunction("smAlloca", intT));
+        smStore = cast<Function>(M.getOrInsertFunction("smStore", voidT, intT, voidPT));
+        smLoad = cast<Function>(M.getOrInsertFunction("smLoad", voidPT, intT));
+        smDeref = cast<Function>(M.getOrInsertFunction("smDeref", voidPPT, intT));
 
         // Loop through all functions
         for (auto &F: M.getFunctionList()) {
@@ -30,17 +42,80 @@ struct CPI : public ModulePass {
         return true;
     }
 
+private:
+    Function *smAlloca;
+    Function *smStore;
+    Function *smLoad;
+    Function *smDeref;  /* Temporarily unused */
+
+    Type *voidT;
+    PointerType *voidPT;
+    PointerType *voidPPT;
+
     void runOnFunction(Function &F) {
-        // Inject test function into main
-        if (F.getName() == "main") {
-            auto &BB = F.getEntryBlock();
-            IRBuilder<> builder(BB.getFirstNonPHI());
-            builder.CreateCall(test);
+        for (auto &bb : F) {
+            auto v = getCPSPtrs(bb);
+            for (auto I : v) {
+                IRBuilder<> b(I);
+
+                // Get index from smAlloca
+                auto idx = b.CreateCall(smAlloca, None, I->getName());
+
+                // Swap out all uses (store and load)
+                for (auto a : I->users()) {
+                    StoreInst *s;
+                    LoadInst *l;
+                    if ((s = dyn_cast<StoreInst>(a))) {
+                        swapStore(idx, s);
+                    } else if ((l = dyn_cast<LoadInst>(a))) {
+                        swapLoad(idx, l);
+                    } else {
+                        /* TODO: Dunno when this will happen, spit logs for now */
+                        DEBUG(dbgs() << *I << "\n");
+                    }
+                }
+                I->eraseFromParent();
+            }
         }
     }
 
-private:
-    Function *test;
+    void swapStore(Instruction *idx, StoreInst *store) {
+        IRBuilder<> b(store);
+        auto cast = b.CreatePointerCast(store->getValueOperand(), voidPT);
+        b.CreateCall(smStore, {idx, cast});
+        store->eraseFromParent();
+    }
+
+    void swapLoad(Instruction *idx, LoadInst *load) {
+        IRBuilder<> b(load);
+        auto raw = b.CreateCall(smLoad, idx);
+        auto cast = b.CreatePointerCast(raw, load->getType());
+        BasicBlock::iterator ii(load);
+        ReplaceInstWithValue(load->getParent()->getInstList(), ii, cast);
+    }
+
+    /* TODO: Might need better CPS pointer detection
+     * Currently only covers function pointer alloca */
+    std::vector<Instruction *> getCPSPtrs(BasicBlock &bb) {
+        std::vector<Instruction *> v;
+        for (Instruction &I : bb) {
+            if (isAllocaFunctionPtr(I)) {
+                DEBUG(dbgs() << "CPS: " << I << "\n");
+                v.push_back(&I);
+            }
+        }
+        return v;
+    }
+
+    bool isAllocaFunctionPtr(Instruction &I) {
+        AllocaInst *i;
+        return (i = dyn_cast<AllocaInst>(&I)) && isFunctionPtr(i->getAllocatedType());
+    }
+
+    bool isFunctionPtr(Type *T) {
+        PointerType *t;
+        return (t = dyn_cast<PointerType>(T)) && t->getElementType()->isFunctionTy();
+    }
 };
 
 char CPI::ID = 0;
--- safe_rt/rt.cpp
+++ safe_rt/rt.cpp
@@ -1,13 +1,31 @@
-#include <iostream>
-
 extern "C" {
 
 // Export as C function symbols
 // Place all function prototypes here
-void test();
+int smAlloca();
+void **smDeref(int idx);
+void *smLoad(int idx);
+void smStore(int idx, void *value);
 
 }
 
-void test() {
-    std::cout << "TEST!!" << std::endl;
+/* TODO ALL: Change to hash table and random index */
+
+static void* pool[100];
+static int cnt = 0;
+
+int smAlloca() {
+    return cnt++;
+}
+
+void **smDeref(int idx) {
+    return &pool[idx];
+}
+
+void smStore(int idx, void *value) {
+    pool[idx] = value;
+}
+
+void *smLoad(int idx) {
+    return pool[idx];
 }
