00efcfb8cbca7514eea0d33a1fc07425ec10057d
topjohnwu <topjohnwu@gmail.com>
2018-12-14 01:25:40 -0500

    Update runtime alloca

--- safe_rt/rt.c
+++ safe_rt/rt.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#define INIT_SZ 4
 
 void **__sm_pool;
 int __sm_sp = 0;
-static int cap = 1;
+static int pool_sz = 0;
 
 int __sm_alloca() {
-    __sm_sp++;
-    if (__sm_pool == NULL || __sm_sp > cap) {
-        cap *= 2;
-        __sm_pool = (void **) realloc(__sm_pool, cap * sizeof(void *));
+    if (__sm_sp == pool_sz) {
+        pool_sz = pool_sz ? pool_sz * 2 : INIT_SZ;
+        __sm_pool = (void **) realloc(__sm_pool, pool_sz * sizeof(void *));
     }
+    // Debug message
+    fprintf(stderr, "__sm_alloca: %d, %d\n", __sm_sp, pool_sz);
     return __sm_sp++;
 }
